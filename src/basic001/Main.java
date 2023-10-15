bd46fd86b7b73d8470bad4fc312181d5e1d2aaf8
minwoo <mark42son@gmail.com>
2023-10-15 22:55:47 +0900

    prac basic quiz 001

---
+++ src/basic001/Main.java
@@ -0,0 +1,12 @@
+package basic001;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student st1 = new Student("손오공", "1682");
+        Student st2 = new Student("저팔계", "4843");
+        Student st3 = new Student("사오정", "5976");
+
+
+    }
+}
